4d7e84952c1ae802cb9dd23f5bcd7ae84dcb59ce
boian <boian@DESKTOP-6DDRDSH>
2022-10-15 23:05:41 +0300

    functions and plot created

---
+++ main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y = 0;
+int fruitX = 0;
+int fruitY = 0;
+int score = 0;
+
+enum eDirection
+{
+    e_STOP = 0,
+    e_LEFT,
+    e_RIGHT,
+    e_UP,
+    e_DOWN
+} dir;
+
+void setUp()
+{
+    gameOver = false;
+    dir = e_STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % height;
+    fruitY = rand() % width;
+    // score=0;
+}
+
+void draw()
+{
+    system("cls");
+
+    char head = '0';
+    char tail = 'o';
+
+    // drawing the top
+    for (int i = 0; i < width; i++)
+    {
+        cout << "# ";
+    }
+    cout << '\n';
+
+    // int newWidth = 2 * width;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+            {
+                cout << "#";
+            }
+            if (j == width - 1)
+            {
+                cout << "#\n";
+            }
+
+            if (j == x && i == y)
+            {
+                cout << head;
+            }
+            else if (i == fruitX && j == fruitY)
+            {
+                cout << 'F';
+            }
+            else
+            {
+                cout << ' ';
+            }
+        }
+    }
+
+    // drawing the bottom
+    for (int i = 0; i < width; i++)
+    {
+        cout << "# ";
+    }
+    cout << '\n';
+}
+
+void input()
+{
+}
+
+void logic()
+{
+}
+
+int main(int argc, char const *argv[])
+{
+    // setUp();
+    // while (!gameOver)
+    // {
+    draw();
+    //     input();
+    //     logic();
+    //     // sleep(10);
+    // }
+
+    return 0;
+}
