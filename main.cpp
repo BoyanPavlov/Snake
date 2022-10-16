6ba37b8463490bd550e983135f35d9e95b2452d2
boian <boian@DESKTOP-6DDRDSH>
2022-10-16 19:43:11 +0300

    working tail

--- main.cpp
+++ main.cpp
@@ -11,6 +11,10 @@ int fruitX = 0;
 int fruitY = 0;
 int score = 0;
 
+int tailX[100] = {};
+int tailY[100] = {};
+int nTail;
+
 enum eDirection
 {
     e_STOP = 0,
@@ -26,8 +30,8 @@ void setUp()
     dir = e_STOP;
     x = width / 2;
     y = height / 2;
-    fruitX = rand() % height;
-    fruitY = rand() % width;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
     // score=0;
 }
 
@@ -46,7 +50,6 @@ void draw()
     }
     cout << '\n';
 
-    // int newWidth = 2 * width;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -66,7 +69,20 @@ void draw()
             }
             else
             {
-                cout << " ";
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << tail;
+                        print = true;
+                    }
+                }
+                if (!print)
+                {
+                    cout << " ";
+                }
             }
 
             if (j == width - 1)
@@ -82,19 +98,92 @@ void draw()
     {
         cout << "#";
     }
-    cout << '\n';
+    cout << "\nScore: " << score << '\n';
 }
 
 void input()
 {
     if (_kbhit()) // if keyboard hit - returns possitive num if is true
     {
-        /* code */
+        switch (_getch()) // get character
+        {
+        case 'w':
+            dir = e_UP;
+            break;
+        case 'a':
+            dir = e_LEFT;
+            break;
+        case 'd':
+            dir = e_RIGHT;
+            break;
+        case 's':
+            dir = e_DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        default:
+            break;
+        }
     }
 }
 
 void logic()
 {
+
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+
+    int prev2X = 0;
+    int prev2Y = 0;
+
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir)
+    {
+    case e_UP:
+        --y;
+        break;
+    case e_DOWN:
+        ++y;
+        break;
+    case e_LEFT:
+        --x;
+        break;
+    case e_RIGHT:
+        ++x;
+        break;
+
+    default:
+        break;
+    }
+
+    if (x > width || x < 0 || y > height || y < 0)
+    {
+        gameOver = true;
+    }
+
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitY = rand() % width;
+        fruitX = rand() % height;
+
+        nTail++;
+    }
 }
 
 int main(int argc, char const *argv[])
